7e50208124edba7640de2c197a5b22167d2b7aa9
Karl Stenerud <kstenerud@gmail.com>
2018-05-12 18:25:53 -0700

    Fix comments on created users

--- utils/lxc-add-guest-users.sh
+++ utils/lxc-add-guest-users.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "lxcnobody:x:165534:" >> /etc/group
-echo "lxcnobody:x:165534:165534:Mapped user for file sharing:/nonexistent:/usr/sbin/nologin" >> /etc/passwd
+echo "lxcnobody:x:165534:165534:Maps to user nobody inside an LXC container:/nonexistent:/usr/sbin/nologin" >> /etc/passwd
 
 echo "lxcfirstuser:x:101000:" >> /etc/group
-echo "lxcfirstuser:x:101000:101000:Mapped user for file sharing:/nonexistent:/usr/sbin/nologin" >> /etc/passwd
+echo "lxcfirstuser:x:101000:101000:Maps to the first user (1000) inside an LXC container:/nonexistent:/usr/sbin/nologin" >> /etc/passwd
