7b20a394f72ad910682802ca758159acdd05ada5
Karl Stenerud <karl.stenerud@canonical.com>
2018-10-16 15:14:31 +0200

    Steam fixes

--- machine-builders/steam/installer.sh
+++ machine-builders/steam/installer.sh
@@ -10,11 +10,13 @@ install_desktop() {
 }
 
 install_other_software() {
+    dpkg --add-architecture i386 && apt update
     install_packages \
         filezilla \
         remmina \
         telnet \
         wine32 \
+        wine64 \
         wine-stable
 }
 
@@ -38,15 +40,24 @@ download_steam()
     popd
 }
 
+add_bin_path()
+{
+    echo "PATH=\"$HOME/bin:$PATH\"" >> /home/$USERNAME/.profile
+}
+
 install_steam_scripts()
 {
+    WINEPREFIX="WINEARCH=win64"
     mkdir -p /home/$USERNAME/bin
-    echo "wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe" >/home/$USERNAME/bin/steam.sh
+    echo "$WINEPREFIX wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe" >/home/$USERNAME/bin/steam.sh
+    chmod a+x /home/$USERNAME/bin/steam.sh
+    echo "$WINEPREFIX winecfg && $WINEPREFIX wine ~/Downloads/SteamSetup.exe" >/home/$USERNAME/Downloads/steamsetup.sh
+    chmod a+x /home/$USERNAME/Downloads/steamsetup.sh
+    echo "-dx10" >/home/$USERNAME/Downloads/kf2_add_to_launch_options.txt
 }
 
 apply_bluetooth_fix
 remove_packages cloud-init
-dpkg --add-architecture i386 && apt update
 create_user $USERNAME $PASSWORD
 install_desktop
 install_remote_desktop $CRD_RESOLUTION
@@ -54,3 +65,5 @@ install_other_software
 disable_unneeded_services
 download_steam
 install_steam_scripts
+add_bin_path
+chown -R $USERNAME:$USERNAME /home/$USERNAME
