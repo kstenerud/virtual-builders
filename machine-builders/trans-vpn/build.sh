0c5740af9b484acb5d40da265f942477a8aac5ac
Karl Stenerud <kstenerud@gmail.com>
2020-03-19 19:52:08 +0000

    Transmission + purevpn

---
+++ machine-builders/trans-vpn/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+SCRIPT_HOME="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+source $SCRIPT_HOME/../../common/lxc-helpers.sh "$SCRIPT_HOME"
+source $SCRIPT_HOME/../../common/options.sh
+set -u
+
+lxc_preconfigure ubuntu 1000 1000 "Transmission + PureVPN." L U
+options_add_switch m path    "Path to mount for downloads" required
+lxc_run_standard_preinstall $@
+lxc_apply_command_line_arguments
+
+MOUNT="$(options_get_existing_directory m)"
+
+lxc_mount_host mirror "$MOUNT" "/var/lib/transmission-daemon/downloads" w
+lxc_run_installer_script
---
+++ machine-builders/trans-vpn/installer.sh
@@ -0,0 +1,9 @@
+set -eu
+
+install_packages transmission-daemon
+
+service transmission-daemon stop
+sed -i 's/"rpc-whitelist": "127.0.0.1"/"rpc-whitelist": "127.0.0.1,192.168.111.*"/g' /etc/transmission-daemon/settings.json
+service transmission-daemon start
+
+install_packages_from_urls https://s3.amazonaws.com/purevpn-dialer-assets/linux/app/purevpn_1.2.3_amd64.deb
