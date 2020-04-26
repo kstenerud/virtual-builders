312fac39d7728e5ec661c654a9abab09e51527e0
Karl Stenerud <kstenerud@gmail.com>
2020-04-26 22:31:44 +0200

    Work is no longer a desktop system

--- machine-builders/work/installer.sh
+++ machine-builders/work/installer.sh
@@ -2,7 +2,6 @@ set -eu
 
 USERNAME="$1"
 PASSWORD="$2"
-CRD_RESOLUTION="$3"
 
 # Do this early to avoid uid/gid brokenness when mapping outside container
 create_user $USERNAME "$PASSWORD"
@@ -11,9 +10,7 @@ remove_packages cloud-init
 install_packages git
 cd /tmp
 git clone --recurse-submodules -j8 https://github.com/kstenerud/work-installer.git
-./work-installer/install-virtual-desktop.sh -r $CRD_RESOLUTION -u $USERNAME 
 ./work-installer/install-dev-software.sh
-./work-installer/install-gui-software.sh
 ./work-installer/add-to-groups.sh $USERNAME
 ./work-installer/install-hostmanager.sh
 rm -rf work-installer
